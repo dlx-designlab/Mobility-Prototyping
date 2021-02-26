256fb74c685c9cb5581fa6af44226cb663434e7b
zkkn <zaksho1121@gmail.com>
2021-02-26 19:41:27 +0900

    update amg test

--- interactive-ripples/tests/AMG8833/amg8833.py
+++ interactive-ripples/tests/AMG8833/amg8833.py
@@ -20,27 +20,32 @@ sensor = adafruit_amg88xx.AMG88XX(i2c_bus)
 time.sleep(.1)
 
 # setup 8x8 and bicubic
-plt.subplots(figsize=(5,5))
+plt.style.use('dark_background')
+plt.subplots(figsize=(8,5))
 
 # start loop
 try:
     while True:
         # get sensor data
+        # preporcessing done in sensro_pos_sender.py
         sensordata = np.array(sensor.pixels)
         #for row in sensordata:
         #    print(["{0:.1f}".format(temp) for temp in row])
         #print("\n")
-        
+
         #Mirror sensor data
-        flip_h = sensordata[:, ::-1]
+        # flip_h =sensordata[:, ::-1]
+        sensordata=np.fliplr(np.flipud(sensordata))
+        extract_sensordata = sensordata[3:]
+        print(extract_sensordata)
+
         #for row in flip_h:
         #    print(["{0:.1f}".format(temp) for temp in row])
         #print("\n")
 
         # show 8x8 data
         plt.subplot(1, 1, 1)
-        fig = plt.imshow(flip_h, cmap="inferno")
-        plt.colorbar()
+        fig = plt.imshow(extract_sensordata, cmap="inferno")
 
         # show bicubic data
         #plt.subplot(1, 2, 2)
